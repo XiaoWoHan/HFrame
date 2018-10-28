9a10ddb8f7a9092f084a01937dd06b6e0e8c3174
Han <www215503346@163.com>
2018-10-29 02:02:34 +0800

    Up

---
+++ CommonModel/Attr/StringLenthAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonModel.Attr
+{
+    public class StringLenthAttribute: Attribute
+    {
+        public string ErrprMsg { get; set; }
+        public int MaxLenth { get; set; }
+        public int MinLenth { get; set; }
+    }
+}
--- CommonModel/MemberModel.cs
+++ CommonModel/MemberModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommonModel.Enum;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommonModel
@@ -8,22 +9,22 @@ namespace CommonModel
         /// <summary>
         /// 用户标识
         /// </summary>
-        [Required(ErrorMessage = "用户标识不可为空")]
-        [StringLength(16,ErrorMessage ="用户标识错误")]
-        public string MemberOID { get; set; }
+        internal string MemberOID { get; set; }
         /// <summary>
         /// 用户名
         /// </summary>
-        [Required(ErrorMessage = "用户名不可为空")]
-        [StringLength(35,MinimumLength = 6, ErrorMessage = "用户名错误")]
-        public string MemberName { get; set; }
+        internal string MemberName { get; set; }
         /// <summary>
         /// 昵称
         /// </summary>
-        public string MemberNiName { get; set; }
+        internal string MemberNickName { get; set; }
         /// <summary>
         /// 登陆标识
         /// </summary>
-        private string Token { get; set; }
+        internal string Token { get; set; }
+        /// <summary>
+        /// 登陆方式
+        /// </summary>
+        internal EnumLoginType LoginType { get; set; }
     }
 }
