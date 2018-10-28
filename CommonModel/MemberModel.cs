3dff0f9f5eeaef1faa92209460b7e9383f53b129
Han <www215503346@163.com>
2018-10-28 13:17:25 +0800

    更改框架

--- CommonModel/MemberModel.cs
+++ CommonModel/MemberModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CommonModel
 {
@@ -7,10 +8,14 @@ namespace CommonModel
         /// <summary>
         /// 用户标识
         /// </summary>
+        [Required(ErrorMessage = "用户标识不可为空")]
+        [StringLength(16,ErrorMessage ="用户标识错误")]
         public string MemberOID { get; set; }
         /// <summary>
         /// 用户名
         /// </summary>
+        [Required(ErrorMessage = "用户名不可为空")]
+        [StringLength(35,MinimumLength = 6, ErrorMessage = "用户名错误")]
         public string MemberName { get; set; }
         /// <summary>
         /// 昵称
