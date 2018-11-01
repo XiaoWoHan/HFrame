39e86e425a5d8da47e636aeb3ad07c7ab3e31c91
韩铭 <WIN10@DESKTOP-7OBIIKJ>
2018-11-01 15:52:27 +0800

    重构，添加Redis管理

---
+++ Common/Helper/RedisHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Helper
+{
+    public class RedisHelper
+    {
+        private static string RedisPath;
+        #region 属性
+        public RedisHelper()
+        {
+            System.Configuration.ConfigurationSettings.AppSettings["RedisPath"];
+        }
+        public RedisHelper(string RedisPath)
+        {
+            this.RedisPath = RedisPath;
+        }
+        #endregion
+    }
+}
---
+++ Common/Helper/TypeHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/* *
+ * 类型帮助类
+ * */
+namespace Common.Helper
+{
+    class TypeHelper
+    {
+        #region 转换时间型
+        /// <summary>
+        /// 将string类型转换成datetime类型
+        /// </summary>
+        /// <param name="s">目标字符串</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns></returns>
+        public static DateTime StringToDateTime(string s, DateTime defaultValue)
+        {
+            if (!string.IsNullOrWhiteSpace(s))
+            {
+                DateTime result;
+                if (DateTime.TryParse(s, out result))
+                    return result;
+            }
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// 将string类型转换成datetime类型
+        /// </summary>
+        /// <param name="s">目标字符串</param>
+        /// <returns></returns>
+        public static DateTime StringToDateTime(string s)
+        {
+            return StringToDateTime(s, DateTime.Now);
+        }
+        #endregion
+    }
+}
