b7c42552e157b241205c0346c6143df5d3a76cb0
韩铭 <WIN10@DESKTOP-7OBIIKJ>
2018-11-02 16:22:21 +0800

    添加枚举帮助类

---
+++ HCommon/Helper/EnumHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCommon.Helper
+{
+    public class EnumHelper
+    {
+        /// <summary>
+        /// 根据枚举的值获取枚举名称
+        /// </summary>
+        /// <typeparam name="T">枚举类型</typeparam>
+        /// <param name="status">枚举的值</param>
+        /// <returns></returns>
+        public static string GetEnumName<T>(int status)
+        {
+            return Enum.GetName(typeof(T), status);
+        }
+        /// <summary>
+        /// 获取枚举名称集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static List<string> GetEnumNames<T>()
+        {
+            return Enum.GetNames(typeof(T)).ToList();
+        }
+        /// <summary>
+        /// 获取枚举名称集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static string[] GetNamesArr<T>()
+        {
+            return Enum.GetNames(typeof(T));
+        }
+        /// <summary>
+        /// 将枚举转换成字典
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Dictionary<string, int> GetDic<T>()where T:struct
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            Type t = typeof(T);
+            var arr = Enum.GetValues(t);
+            foreach (var item in arr)
+            {
+                dic.Add(item.ToString(), (int)item);
+            }
+
+            return dic;
+        }
+        /// <summary>
+        /// 获取指定枚举成员的描述
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToDescriptionString(Enum obj)
+        {
+            var attribs = (DescriptionAttribute[])obj.GetType().GetField(obj.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attribs.Length > 0 ? attribs[0].Description : obj.ToString();
+        }
+    }
+}
