3a83b866d1e0e05115d1d85235504feb0ccbaf8b
韩铭 <WIN10@DESKTOP-7OBIIKJ>
2018-11-05 13:36:10 +0800

    添加Web视图，Db数据库

---
+++ HFrame.Web/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HFrame.Web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+
+            return Json();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
